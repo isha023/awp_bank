121cae1cc7f19bc283ce4cda671a63a4ff29d79b
isha023 <117734909+isha023@users.noreply.github.com>
2023-10-17 21:47:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,54 @@
 <?php
 require 'class_bank_account.php';
+include 'db_config.php';
 
-$bank = new bank_account($conn); //Person from class in the person.php line 4,   $person was the variablke going to used in calling
+//Person from class in the person.php line 4,   $person was the variablke going to used in calling
 
 // Handle form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  
-    } elseif (isset($_POST['deposit'])) {
-        $account_id = $_POST['deposit_id'];
-        $amount = $_POST['deposit_amount'];
-        $bank->deposit($account_id, $amount);
-    } elseif (isset($_POST['withdraw'])) {
-        $account_id = $_POST['withdraw_id'];
-        $amount = $_POST['withdraw_amount'];
-        $bank->withdraw($account_id, $amount);
-    }
+    $account_id = $_POST['account_id'];
+  $action = $_POST['action'];
+  $amount = $_POST['amount'];
 
-// Read all persons
-$banks = $bank->readaccounts();
+  $bank = new bank_account($account_id, null, 0, null, null); 
+    
+  if ($action === "inquire") {
+    $balance = $bank->inquire();
+    echo "Current Balance: $balance";
+  } elseif ($action === "deposit") {
+    $result = $bank->deposit($amount);
+    echo $result;
+  } elseif ($action === "withdraw") {
+    $result = $bank->withdraw($amount);
+    echo $result;
+  }
+}
+$sql = "SELECT account_id,account_name, balance, account_type FROM bank_account";
+$result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+  echo '<h2>Accounts</h2>';
+  echo '<table border="1">';
+  echo '<tr>
+  <th>Account Id</th>
+    <th>Account Name</th>
+    <th>Account Type</th>
+    <th>Balance</th>
+    
+  </tr>';
+
+  while ($row = $result->fetch_assoc()) {
+    echo '<tr>';
+    echo '<td>' . $row['account_id'] . '</td>';
+    echo '<td>' . $row['account_name'] . '</td>';
+    echo '<td>' . $row['account_type'] . '</td>';
+    echo '<td>' . $row['balance'] . '</td>';
+    
+    echo '</tr>';
+  }
+
+  echo '</table>';
+}
 mysqli_close($conn);
 ?>
 
@@ -31,38 +61,25 @@ mysqli_close($conn);
     
 
    
-    <h2>Accounts</h2>
-    <table border="1">
-        <tr>
-            <th>Account Name</th>
-            <th>Account Type</th>
-            <th>Balance</th>
-            <th>Actions</th>
-        </tr>
-        <?php foreach ($banks as $bank) { ?>
-        <tr>
-            <td><?php echo $bank['account_name']; ?></td>
-            <td><?php echo $bank['account_type']; ?></td>
-            <td><?php echo $bank['balance']; ?></td>
-            <td>
-                <form method="POST" action="index.php">
-                    <input type="hidden" name="deposit_id" value="<?php echo $bank['account_id']; ?>">
-                    <input type="text" name="deposit_amount" placeholder="amount to deposit">
-                    <input type="submit" name="deposit" value="Deposit">
-                </form>
-                <form method="POST" action="index.php">
-                <input type="hidden" name="withdraw_id" value="<?php echo $bank['account_id']; ?>">
-                    <input type="text" name="withdraw_amount" placeholder="amount to withdraw">
-                    <input type="submit" name="withdraw" value="Withdraw">
-                </form>
-                <form method="POST" action="index.php">
-                <input type="hidden" name="inquire_id" value="<?php echo $bank['account_id']; ?>">
-                    <input type="text" name="current_balance">
-                    <input type="submit" name="inquire" value="Inquire">
-                </form>
-            </td>
-        </tr>
-        <?php } ?>
-    </table>
+    
+   
+    <h2>Bank Operations</h2>
+  <form action="index.php" method="post">
+    <label for="account_id">Account Id:</label>
+    <input type="text" name="account_id" id="account_id" required><br>
+
+    <label for="action">Action:</label>
+    <select name="action" id="action">
+      <option value="deposit">Deposit</option>
+      <option value="withdraw">Withdraw</option>
+      <option value="inquire">Inquire</option>
+    </select><br>
+
+    <label for="amount">Amount:</label>
+    <input type="text" name="amount" id="amount"><br>
+
+    <input type="submit" value="Submit">
+  </form>
+
 </body>
 </html>
